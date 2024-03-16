68dedd60b9b52d9278efca2ef41f4af6045ceb50
DanDappa <93945794+DappaDanDev@users.noreply.github.com>
2024-03-16 19:33:51 +0000

    back to working frontend

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -7,6 +7,12 @@ import { WagmiConfig, configureChains, createConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
+import { Noir } from '@noir-lang/noir_js';
+import noir_circuit from '../../circuit/target/ciruit.json';
+import React, { useEffect, useState } from 'react';
+
+
 
 const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string; // get one at https://cloud.walletconnect.com/app
 
@@ -31,11 +37,51 @@ const wagmiConfig = createConfig({
 });
 
 function App({ Component, pageProps }: AppProps) {
+  const [logs, setLogs] = useState<string[]>([]);
+  const [results, setResults] = useState<Uint8Array | null>(null);
+  const [inputX, setInputX] = useState(0);
+  const [inputY, setInputY] = useState(0);
+
+  const handleGenerateProof = async () => {
+    const backend = new BarretenbergBackend(noir_circuit as any);
+    const noir = new Noir(noir_circuit as any, backend);
+    const input = { x: inputX, y: inputY };
+
+    setLogs((logs) => [...logs, 'Generating proof... ⌛']);
+    const proof = await noir.generateFinalProof(input);
+
+    setLogs((logs) => [...logs, 'Generating proof... ✅']);
+    setResults(proof.proof);
+
+    setLogs((logs) => [...logs, 'Verifying proof... ⌛']);
+    const verification = await noir.verifyFinalProof(proof);
+
+    if (verification) {
+      setLogs((logs) => [...logs, 'Verifying proof... ✅']);
+    }
+  };
+
+
+
+
   return (
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains} appInfo={appInfo} coolMode={true}>
         <Layout>
           <Component {...pageProps} />
+          <div>
+      <input type="number" value={inputX} onChange={(e) => setInputX(e.target.value)} placeholder="Enter value for x" />
+      <input type="number" value={inputY} onChange={(e) => setInputY(e.target.value)} placeholder="Enter value for y" />
+      <button onClick={handleGenerateProof}>Generate Proof</button>
+      <div id="logs">
+        {logs.map((log, index) => (
+          <p key={index}>{log}</p>
+        ))}
+      </div>
+      <div id="results">
+        <p>{results}</p>
+      </div>
+    </div>
         </Layout>
       </RainbowKitProvider>
     </WagmiConfig>
